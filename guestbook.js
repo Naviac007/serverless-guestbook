a2ba900a8dcc0aee23f3ab0748a6cab57e759bf5
Naviac007 <naviac007@gmail.com>
2021-12-29 14:39:33 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://7c884fd0.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
